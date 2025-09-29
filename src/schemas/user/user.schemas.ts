3fc907aee4b12f1dc16da1d0a982bd0bf807b018
Nathan <viniciosnathan@gmail.com>
2025-09-29 15:57:26 -0300

    feat: add zod validation schemas for user register and login

---
+++ src/schemas/user/user.schemas.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const createUserSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+});
+
+export const loginUserSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
